707846b9c0aa921900c33dddf3f522dd47271426
Caio Toledo <caiobbgtoledo@gmail.com>
2025-06-25 23:43:34 -0300

    testing use cases

--- src/@core/events/domain/entities/__tests__/event.entity.spec.ts
+++ src/@core/events/domain/entities/__tests__/event.entity.spec.ts
@@ -28,3 +28,37 @@ test('should create an event', () => {
   expect(event.total_spots_reserved).toBe(0);
   expect(event.partner_id.value).toBe('f9eb93a0-c07a-4f7d-b0ff-6bb2f42c752a');
 });
+
+test('should publish all itens of event', () => {
+  const event = Event.create({
+    name: 'Test Event',
+    description: 'This is a test event',
+    date: new Date('2023-10-01'),
+    partner_id: new PartnerId('f9eb93a0-c07a-4f7d-b0ff-6bb2f42c752a'),
+  });
+
+  event.addSection({
+    name: 'Test Section 1',
+    description: 'This is a test section',
+    total_spots: 100,
+    price: 50,
+  });
+
+  event.addSection({
+    name: 'Test Section 2',
+    description: 'This is a test section',
+    total_spots: 100,
+    price: 50,
+  });
+
+  event.publishAll();
+
+  expect(event.is_published).toBe(true);
+  const [section1, section2] = event.sections;
+  expect(section1.is_published).toBe(true);
+  expect(section2.is_published).toBe(true);
+
+  [...(section1.spots ?? []), ...(section2.spots ?? [])].forEach((spot) => {
+    expect(spot.is_published).toBe(true);
+  });
+});
