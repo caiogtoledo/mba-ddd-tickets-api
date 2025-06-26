b5e3407dea8128204b4ba910514f17bbdaac8a08
Caio Toledo <caiobbgtoledo@gmail.com>
2025-06-25 23:19:40 -0300

    testing create event with spots

--- src/@core/events/domain/entities/__tests__/event.entity.spec.ts
+++ src/@core/events/domain/entities/__tests__/event.entity.spec.ts
@@ -10,22 +10,21 @@ test('should create an event', () => {
     partner_id: new PartnerId('f9eb93a0-c07a-4f7d-b0ff-6bb2f42c752a'),
   });
 
-  const section = EventSection.create({
+  event.addSection({
     name: 'Test Section',
     description: 'This is a test section',
     total_spots: 100,
     price: 50,
   });
 
-  //   event.addSection(section);
-  event.sections.add(section);
-
   expect(event).toBeInstanceOf(Event);
   expect(event.name).toBe('Test Event');
   expect(event.description).toBe('This is a test event');
   expect(event.date).toEqual(new Date('2023-10-01'));
   expect(event.is_published).toBe(false);
-  expect(event.total_spots).toBe(0);
+  expect(event.total_spots).toBe(100);
+  const [section] = event.sections;
+  expect(section.spots?.size ?? 0).toBe(100);
   expect(event.total_spots_reserved).toBe(0);
   expect(event.partner_id.value).toBe('f9eb93a0-c07a-4f7d-b0ff-6bb2f42c752a');
 });
