4554f2de73c5c488873c12a82fd662e72774e8a5
Caio Toledo <caiobbgtoledo@gmail.com>
2025-06-25 22:31:50 -0300

    event section

--- src/@core/events/domain/entities/event-section.ts
+++ src/@core/events/domain/entities/event-section.ts
@@ -1,24 +1,66 @@
-import { Entity } from "../../../shared/domain/entity";
-import Uuid from "../../../shared/domain/value-objects/uuid.vo";
+import { Entity } from '../../../shared/domain/entity';
+import Uuid from '../../../shared/domain/value-objects/uuid.vo';
 
 export class EventSectionId extends Uuid {}
 
 export type EventSectionCreateCommand = {
-    name: string;
-    description?: string | null;
-    total_spots: number;
-    price: number;
+  name: string;
+  description?: string | null;
+  total_spots: number;
+  price: number;
+};
+
+export type EventSectionConstructorProps = {
+  id?: EventSectionId | string;
+  name: string;
+  description?: string | null;
+  is_published?: boolean;
+  total_spots: number;
+  total_spots_reserved: number;
+  price: number;
+};
+
+export class EventSection extends Entity<EventSectionConstructorProps> {
+  id: EventSectionId;
+  name: string;
+  description: string | null;
+  is_published: boolean;
+  total_spots: number;
+  total_spots_reserved: number;
+  price: number;
+
+  constructor(props: EventSectionConstructorProps) {
+    super();
+    this.id =
+      typeof props.id === 'string'
+        ? new EventSectionId(props.id)
+        : (props.id ?? new EventSectionId());
+    this.name = props.name;
+    this.description = props.description ?? null;
+    this.is_published = props.is_published ?? false;
+    this.total_spots = props.total_spots;
+    this.total_spots_reserved = props.total_spots_reserved ?? 0;
+    this.price = props.price;
+  }
+
+  static create(command: EventSectionCreateCommand) {
+    return new EventSection({
+      ...command,
+      description: command.description ?? null,
+      is_published: false,
+      total_spots_reserved: 0,
+    });
+  }
+
+  toJSON() {
+    return {
+      id: this.id.value,
+      name: this.name,
+      description: this.description,
+      is_published: this.is_published,
+      total_spots: this.total_spots,
+      total_spots_reserved: this.total_spots_reserved,
+      price: this.price,
+    };
+  }
 }
-
-export type EventSectionConstructor = {
-
-}
-
-// export class EventSection extends Entity {
-//     id: EventSectionId;
-//     name: string;
-//     description: string;
-//     is_published: boolean;
-//     total_spots: number;
-//     total_spots_reserved: number;
-// }
