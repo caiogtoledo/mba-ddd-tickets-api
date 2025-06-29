8029cf902b3e4570044b44a26cae0a1b6c11c9bc
Caio Toledo <caiobbgtoledo@gmail.com>
2025-06-29 20:04:22 -0300

    partner repo impl mysql mikro orm

---
+++ src/@core/events/infra/db/repositories/partner-mysql.repository.ts
@@ -0,0 +1,28 @@
+import {
+  Partner,
+  PartnerId,
+} from '../../../../events/domain/entities/partner.entity';
+import { PartnerRepositoryInterface } from '../../../../events/domain/repositories/partner.repository.interface';
+import { EntityManager } from '@mikro-orm/mysql';
+
+export class PartnerMysqlRepository implements PartnerRepositoryInterface {
+  constructor(private readonly em: EntityManager) {}
+
+  async add(entity: Partner): Promise<void> {
+    this.em.persist(entity);
+  }
+
+  async findById(id: string | PartnerId): Promise<Partner | null> {
+    return this.em.findOne(Partner, {
+      id: typeof id === 'string' ? new PartnerId(id) : id,
+    });
+  }
+
+  async findAll(): Promise<Partner[]> {
+    return this.em.find(Partner, {});
+  }
+
+  async delete(entity: Partner): Promise<void> {
+    this.em.remove(entity);
+  }
+}
