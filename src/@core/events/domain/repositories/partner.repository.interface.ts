eb7fccb543c893ca42911512544db6dd9324ebc4
Caio Toledo <caiobbgtoledo@gmail.com>
2025-06-29 20:03:58 -0300

    partner repo interface

---
+++ src/@core/events/domain/repositories/partner.repository.interface.ts
@@ -0,0 +1,4 @@
+import { IRepository } from '../../../shared/domain/repository-interface';
+import { Partner } from '../entities/partner.entity';
+
+export interface PartnerRepositoryInterface extends IRepository<Partner> {}
